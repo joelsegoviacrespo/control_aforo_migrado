3c8b358cc790e555b864a975023d13fdeadec244
Raúl <68138752+hernandez20@users.noreply.github.com>
2020-08-22 15:28:24 -0400

    cambio de "{% out %}" por el enlace de una imagen

--- core/views.py
+++ core/views.py
@@ -1,17 +1,17 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-import requests
+
 
 from templates.includes import index_base 
-
-def post(self, request):
+class HomeView(TemplateView):
+    template_name ='include/index_base.html'
+    def post(self, request):
  
-    headers = {
-        'X-Cisco-Meraki-API-Key': '920a310b87feb3832739a79d573845404c6825d0',
-        'Content-Type': 'application/json',
-    }
+        headers = {
+            'X-Cisco-Meraki-API-Key': '920a310b87feb3832739a79d573845404c6825d0',
+            'Content-Type': 'application/json',
+        }
 
-    response = requests.post('https://api.meraki.com/api/v1/devices/Q2HV-B24V-ZKN5/camera/generateSnapshot', headers=headers)
-
-    return render(request, 'index_base.html',{'out':response})
+       response = requests.post('https://api.meraki.com/api/v1/devices/Q2HV-B24V-ZKN5/camera/generateSnapshot', headers=headers)
+       return render(request, template_name,{'out':response})
 
