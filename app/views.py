1cda848dc3ad8e31114f92bae8a913fb138cba05
Gabriel Lara <gabriel.alejandro.lara@gmail.com>
2020-09-10 14:55:29 -0400

    Print forme
    asdasd

--- app/views.py
+++ app/views.py
@@ -143,6 +143,7 @@ def index(request):
 
                                 #print(result, flush=True)  
                                 #print(response2.text.encode('utf8', {'flush': form2})) 
+                                print (formeee)
                         return render(request, "index.html",  {'formee': formeee,'camaras':camarasAll})
                                 
 
