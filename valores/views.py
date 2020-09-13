b21b59d667ff4535f37967b3809e0c7fbb84927a
joel segovia <joelsegoviacrespo@gmail.com>
2020-09-12 20:30:48 -0400

    se agrego la permisologia para el modulo de valores

--- valores/views.py
+++ valores/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render, redirect, get_object_or_404, get_list_or_40
 from django.utils.translation import activate
 from valores.forms import ValoresForm,ValoresEditarForm
 from valores.models import Valores
+import Constantes
 
 
 @login_required(login_url="/login/")
@@ -39,18 +40,29 @@ def valores(request):
 def todos(request):
     activate('es')
     valoresTodos = {}
-    #if request.user.is_staff:
-    valoresTodos = Valores.objects.all()
+    
+    if (request.user.profile.rol== Constantes.SUPERUSUARIO):
+        valoresTodos = Valores.objects.all()
         
-    #elif hasattr(request.user, 'cliente') and (request.user.cliente.get_id() is not None):
-        
-    #    instalaciones  = Instalacion.objects.filter(id_cliente=request.user.cliente.get_id())
+    elif (request.user.profile.rol == Constantes.ADMINISTRADOR) and hasattr(request.user.profile, 'cliente') and (request.user.profile.cliente.get_id() is not None):
+            print("Perfil Administrador")    
+            print("nif del cliente")
+            print(request.user.profile.cliente.nif)
+            valoresTodos = Valores.objects.all().filter(instalacion={'nif_cliente': request.user.profile.cliente.nif})
+
+    elif (request.user.profile.rol > Constantes.ADMINISTRADOR) and hasattr(request.user.profile, 'cliente') and (request.user.profile.cliente.get_id() is not None): 
+          print('nif_cliente: ',request.user.profile.cliente.nif)
+          print('nombre: ',request.user.profile.instalacion.nombre_comercial)
+          if hasattr(request.user.profile, 'instalacion') and (request.user.profile.instalacion.get_id() is not None):
+              valoresTodos = Valores.objects.all().filter(instalacion={'nif_cliente': request.user.profile.cliente.nif} and {'nombre': request.user.profile.instalacion.nombre_comercial})
+
+            
+   
 
     for valores in valoresTodos:
         valores.id = str(valores._id)
 
     return render(request, "valores/todos.html", {'valoresTodos': valoresTodos})
-
 
 @login_required(login_url="/login/")
 def editar(request, id):
